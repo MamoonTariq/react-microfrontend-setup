5fa34bf261a708076fa01d0f6f67534a3d363b60
unknown <m.mamoon.tariq@gmail.com>
2022-07-26 00:53:35 +0500

    added button

--- shared/src/App.tsx
+++ shared/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "./Components";
+
 function AppCore() {
-  return <div className="App">Core Shared</div>;
+  return (
+    <div className="App">
+      Core Shared <Button />
+    </div>
+  );
 }
 
 export default AppCore;
